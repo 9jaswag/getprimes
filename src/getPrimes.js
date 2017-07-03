34e8a528264d16d16678502f3e4fe69ff83ca635
Chuks Opia <Chuks24ng@yahoo.co.uk>
2017-07-03 15:30:46 +0100

    Export function

--- src/getPrimes.js
+++ src/getPrimes.js
@@ -1,8 +1,34 @@
 'use strict'
 
-module.exports = {
-    // find primes from 0 to integer n
-    getPrimes: (n) => {
-        
+// find primes from 0 to integer n
+function getPrimes (n) {
+    // declare primes variable
+    let primes = [];
+    // handle primes
+    if (!(Number.isInteger(n)) || Math.sign(n) == -1 || n == 0 || n == 1) {
+        return "undefined";
+    } else {
+        for (let i = 2; i <= n; i++) {
+            if(isPrime(i)) {
+                primes.push(i);
+            }
+        }
+        return primes
     }
+}
+
+// function to check if a number is a prime
+function isPrime (n) {
+    let limit = Math.sqrt(n);
+    let prime = true;
+    for (let i = 2; i <= limit; i++) {
+        if (n%i == 0) {
+            prime = false;
+        }
+    }
+    return prime;
+}
+
+module.exports = {
+    getPrimes: getPrimes
 }
--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,20 @@
 const assert = require('chai').assert;
-const myApp = require('../src/getPrime.js');
+const myApp = require('../src/getPrimes.js');
 
 describe ('getPrimes', function(){
 
     describe('handle valid input', function(){
         it ('should return [2, 3, 5, 7] as prime of 7', function(){
-            assert.equal(myApp.getPrimes(7), [2, 3, 5, 7]);
+            assert.deepEqual(myApp.getPrimes(7), [2, 3, 5, 7]);
         });
-        it ('should return [2, 3, 5, 7, 11, 13, 17] as prime of 7', function(){
-            assert.equal(myApp.getPrimes(17), [2, 3, 5, 7, 11, 13, 17]);
+        it ('should return [2, 3, 5, 7, 11, 13, 17] as prime of 17', function(){
+            assert.deepEqual(myApp.getPrimes(17), [2, 3, 5, 7, 11, 13, 17]);
+        });
+        it ('should return [2, 3, 5, 7, 11, 13, 17, 19, 23] as prime of 24', function(){
+            assert.deepEqual(myApp.getPrimes(24), [2, 3, 5, 7, 11, 13, 17, 19, 23]);
+        });
+        it ('should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61] as prime of 62', function(){
+            assert.deepEqual(myApp.getPrimes(62), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61]);
         });
     })
 
@@ -16,17 +22,17 @@ describe ('getPrimes', function(){
         it ('should return undefined as prime of 0', function(){
             assert.equal(myApp.getPrimes(0), 'undefined');
         });
-        it ('should return 1 is not a prime for 1', function(){
-            assert.equal(myApp.getPrimes(1), '1 is not a prime');
+        it ('should return undefined for 1', function(){
+            assert.equal(myApp.getPrimes(1), 'undefined');
         });
-        it ('should return negative numbers do not have primes for -5', function(){
-            assert.equal(myApp.getPrimes(-5), 'negative numbers do not have primes');
+        it ('should return undefined for -5', function(){
+            assert.equal(myApp.getPrimes(-5), 'undefined');
         });
-        it ('should return only positive numbers allowed for "string"', function(){
-            assert.equal(myApp.getPrimes('string'), 'only positive numbers allowed');
+        it ('should return undefined for "string"', function(){
+            assert.equal(myApp.getPrimes('string'), 'undefined');
         });
-        it ('should return only positive numbers allowed for ""', function(){
-            assert.equal(myApp.getPrimes(''), 'only positive numbers allowed');
+        it ('should return undefined for ""', function(){
+            assert.equal(myApp.getPrimes(''), 'undefined');
         });
     })
 
